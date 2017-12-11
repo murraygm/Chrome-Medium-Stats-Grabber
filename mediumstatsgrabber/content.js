d9639956218f77136d72d20eed2b7f564b35d47e
Murray GM <murray.gm@gmail.com>
2017-12-11 10:18:47 -0800

    added pub date and fixed no publication issue

--- mediumstatsgrabber/content.js
+++ mediumstatsgrabber/content.js
@@ -20,28 +20,34 @@ chrome.runtime.onMessage.addListener(
     if( request.message === "clicked_browser_action" ) {
 
     	var URLBits = document.URL;
-		console.log(document.URL);
+		//console.log(document.URL);
 
 
 
 		if(URLBits.includes("overview")){
-			alert("Please select the 'Stories' view and try again")
+			alert("Please select the 'Stories' view and try again");
 		} else {;
 
-	    	content = "mediumID|title|link|publication|mins|views|reads|readRatio|fans"+ "\n";
-			rows = document.querySelectorAll(".sortableTable-row.js-statsTableRow")
+	    	var content = "mediumID|title|link|publication|mins|views|reads|readRatio|fans|pubDate"+ "\n";
+			var rows = document.querySelectorAll(".sortableTable-row.js-statsTableRow");
 			rows.forEach(function(row) {
-				mediumID = row.getAttribute("data-action-value")
-				title = row.querySelectorAll("h2.sortableTable-title")[0].innerText
-				link = row.querySelectorAll("a.sortableTable-link")[1].href
-				publication = row.querySelectorAll("a.sortableTable-link")[0].innerText
-				mins = row.querySelectorAll("span.readingTime")[0].title
-				values = row.querySelectorAll(".sortableTable-value")
-				views = values[1].innerText
-				reads = values[2].innerText
-				readRatio = values[3].innerText
-				fans = values[4].innerText
-				content += mediumID + "|" + title + "|" + link + "|" + publication + "|" + mins + "|" + views + "|" + reads + "|" + readRatio + "|" + fans + "\n"
+				var mediumID = row.getAttribute("data-action-value");
+				var title = row.querySelectorAll("h2.sortableTable-title")[0].innerText;
+				var link = row.querySelectorAll("a.sortableTable-link")[1].href;
+				var publicationtxt = row.querySelectorAll("a.sortableTable-link")[0].innerText;
+				if(publicationtxt == "View story"){
+					var publication = "Not in publication";
+				} else {
+					var publication = publicationtxt;
+				};
+				var mins = row.querySelectorAll("span.readingTime")[0].title;
+				var values = row.querySelectorAll(".sortableTable-value");
+				var views = values[1].innerText;
+				var reads = values[2].innerText;
+				var readRatio = values[3].innerText;
+				var fans = values[4].innerText;
+				var pubDate = new Date(parseInt(row.getAttribute("data-timestamp"))).toISOString().slice(0, 10);
+				content += mediumID + "|" + title + "|" + link + "|" + publication + "|" + mins + "|" + views + "|" + reads + "|" + readRatio + "|" + fans + "|" + pubDate +"\n";
 			});
 
 			
@@ -52,10 +58,10 @@ chrome.runtime.onMessage.addListener(
 			if(statType.innerText == "Stats"){
 				statsfrom = "MyStats"; 
 			} else {
-				statsfrom = document.querySelectorAll("h1.hero-title")[0].innerText
+				statsfrom = document.querySelectorAll("h1.hero-title")[0].innerText;
 			};
 			
-			download(statsfrom + "-medium-metrics-" + new Date().toISOString().slice(0, 10) + ".csv", content)
+			download(statsfrom + "-medium-metrics-" + new Date().toISOString().slice(0, 10) + ".csv", content);
 		};
     }
   }
